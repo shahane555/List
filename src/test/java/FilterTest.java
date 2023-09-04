52602cd13758ede572e108f627ebd4a0ed485b1b
Emma Paris <shahane.manucharyan@mail.ru>
2023-09-04 09:47:35 +0300

    Separate different tests

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -11,12 +11,7 @@ public class FilterTest extends DriverSetup {
     FilterResultsPage filterResultsPage;
 
     @Test
-    public void filterTest() {
-        homePage.selectLanguage("English");
-        Category category = new Category(driver, action, wait);
-        category.chooseCategory("Electronics", "Computers", "Notebooks");
-        category.chooseFromDropDown("Location", "Kentron");
-        category.setPrice("200000", "500000");
+    public void filter () {
         filterResultsPage = new FilterResultsPage(driver, "/category/116?n=8&bid=0&price1=200000&price2=500000&crc=&_a24=0");
         filterResultsPage.open();
         List<FilteredItem> filteredItems = filterResultsPage.getAllSearchedItems();
@@ -25,5 +20,14 @@ public class FilterTest extends DriverSetup {
             Assert.assertTrue(searchItem.IsItemPriceLessThan(500000), "Actual price is more than expected price");
             Assert.assertTrue(searchItem.isItemPriceMoreThan(200000), "Actual price is less than expected price");
         }
+    }
+
+    @Test
+    public void chooseCategory() {
+        homePage.selectLanguage("English");
+        Category category = new Category(driver, action, wait);
+        category.chooseCategory("Electronics", "Computers", "Notebooks");
+        category.chooseFromDropDown("Location", "Kentron");
+        category.setPrice("200000", "500000");
     }
 }
